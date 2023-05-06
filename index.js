60f6ecc34ce3692ee18f9c868e8a0212ec27c5a1
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2023-05-06 20:41:10 +0530

    new

--- index.js
+++ index.js
@@ -28,20 +28,6 @@ app.post(
     })
 );
 
-// Your code
-if (process.env.NODE_ENV === "production") {
-    const path = require("path");
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
-            if(err) {
-                res.status(500).send(err)
-            }
-        });
-    })
-}
-// Your code
-
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use('/uploads', express.static(__dirname + '/uploads'));
