0c1a972b0b9fec0a7c91bdd3cd320e49cbe92e15
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2023-05-06 20:23:01 +0530

    new commit

--- pack/src/scenes/routes/UserPrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, useRevalidator } from 'react-router-dom'
-import { useSelector } from 'react-redux';
-
-const UserPrivateRoute = ({ children }) => {
-    const { user } = useSelector((state) => state.authReducer);
-    return user ? children : <Navigate to="/" />
-}
-
-export default UserPrivateRoute;
